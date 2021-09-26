eceea14d7828fec5bca57715912ca914c232c493
Guitarja <124254388@qq.com>
2021-09-26 00:55:08 -0500

    Time: 780 ms (80.47%), Space: 27.9 MB (43.05%) - LeetHub

---
+++ largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
@@ -0,0 +1,16 @@
+class Solution:
+    def largestRectangleArea(self, heights: List[int]) -> int:
+        heights.append(0) # here to make sure stack pop off
+        stack = [-1]
+        ans = 0
+        for i in range(len(heights)):
+            while heights[i] < heights[stack[-1]]:
+                h = heights[stack.pop()]
+                w = i - stack[-1] - 1
+                ans = max(ans, h * w)
+            
+            stack.append(i)
+        
+        heights.pop()
+        return ans
+    '''The stack maintain the indexes of buildings with ascending height. Before adding a new building pop the building who is taller than the new one. The building popped out represent the height of a rectangle with the new building as the right boundary and the current stack top as the left boundary. Calculate its area and update ans of maximum area. Boundary is handled using dummy buildings.'''

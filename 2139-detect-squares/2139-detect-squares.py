18738dd745af72cf1364167a34145530dcdec383
Guitarja <124254388@qq.com>
2023-08-24 15:41:38 -0500

    Time: 320 ms (68.26%), Space: 18.3 MB (68.99%) - LeetHub

--- 2139-detect-squares/2139-detect-squares.py
+++ 2139-detect-squares/2139-detect-squares.py
@@ -1,4 +1,11 @@
 class DetectSquares:
+    '''
+    To compute count(p1):
+    We try all points p3 which together with p1 form the diagonal of non-empty square, it means abs(p1.x-p3.x) == abs(p1.y-p3.y) && abs(p1.x-p3.x) > 0
+    Since we have 2 points p1 and p3, we can form a square by computing the positions of 2 remain points p2, p4.
+    p2 = (p1.x, p3.y)
+    p4 = (p3.x, p1.y)
+    '''
     def __init__(self):
         self.cntPoints = Counter()
 
@@ -13,6 +20,13 @@ class DetectSquares:
                 continue  # Skip empty square or invalid square point!
             ans += cnt * self.cntPoints[(x1, y3)] * self.cntPoints[(x3, y1)]
         return ans
+
+# Complexity
+
+# Time:
+# add: O(1)
+# count: O(T), where T <= 5000 is total number of points after calling add.
+# Space: O(T) in Python, O(1000^2) in C++/Java
 
 # Your DetectSquares object will be instantiated and called as such:
 # obj = DetectSquares()

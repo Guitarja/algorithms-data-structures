c78d193e4c883ad2282137000683e9262407b4a2
Guitarja <124254388@qq.com>
2023-09-18 03:30:41 -0500

    Time: 35 ms (78.31%), Space: 16.3 MB (62.58%) - LeetHub

---
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.py
@@ -0,0 +1,20 @@
+class Solution:
+    def numTrees(self, n: int) -> int:
+        dp = [0 for _ in range(n+1)]
+        dp[0] = 1 #base case, if tree is empty
+        dp[1] = 1 #if only one node
+        
+        for i in range(2, n + 1):
+            for j in range(1, i + 1):
+                dp[i] += dp[j-1] * dp[i-j]
+        
+        return dp[-1]
+
+# F(i,n)=G(i−1)⋅G(n−i)
+
+# G(n)=∑i-n G(i−1)⋅G(n−i)
+
+# Complexity Analysis
+
+# Time complexity : n^2
+# Space complexity : O(N)

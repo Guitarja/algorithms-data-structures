fd857b0834770080240d4a4bad3250cf5dd9b66c
Guitarja <124254388@qq.com>
2023-10-06 02:28:33 -0500

    Time: 78 ms (44.32%), Space: 17 MB (21.52%) - LeetHub

---
+++ 1184-car-pooling/1184-car-pooling.py
@@ -0,0 +1,22 @@
+class Solution:
+    def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
+        for time, passenger in sorted([item for numPassengersi, fromi, toi in trips
+                                      for item in [[fromi, numPassengersi], [toi, -numPassengersi]]]
+                                      ,key =lambda x: (x[0],x[1])):
+            capacity -= passenger
+            if capacity < 0:
+                return False
+        print(sorted([item for numPassengersi, fromi, toi in trips
+                                      for item in [[fromi, numPassengersi], [toi, -numPassengersi]]]
+                                      ,key =lambda x: (x[0],x[1])))
+        return True
+    
+    '''Explanation
+Save all time points and the change on current capacity
+Sort all the changes on the key of time points.
+Track the current capacity and return false if negative
+Complexity
+Time O(NlogN)
+Space O(N)
+
+'''

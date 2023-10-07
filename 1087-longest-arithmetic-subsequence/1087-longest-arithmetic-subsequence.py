3e4276f301c064558b062db64888d18c6e683dff
Guitarja <124254388@qq.com>
2023-10-07 11:12:31 -0500

    Time: 2210 ms (64.67%), Space: 68.8 MB (14.45%) - LeetHub

---
+++ 1087-longest-arithmetic-subsequence/1087-longest-arithmetic-subsequence.py
@@ -0,0 +1,9 @@
+class Solution:
+    def longestArithSeqLength(self, A: List[int]) -> int:
+        cumu = {}
+        for i in range(len(A)):
+            for j in range(i + 1, len(A)):
+                # i不变在每一遍遍历
+                cumu[j, A[j] - A[i]] = cumu.get((i, A[j] - A[i]), 1) + 1
+        #print(cumu.items())
+        return max(cumu.values())

149d5e54d45816c2e5cf1783b3242669c786826f
Guitarja <124254388@qq.com>
2023-10-11 03:26:54 -0500

    Time: 41 ms (72.06%), Space: 16.3 MB (71.28%) - LeetHub

---
+++ 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findTheWinner(self, n: int, k: int) -> int:
+        p = 1
+        for i in range(1,n):
+            # here i represent number of alive people
+            # p is f(i,'cac')
+            p=(k+p-1)%(i+1)+1
+            # p is f(i+1, 'cac')
+        return p
+# https://leetcode.com/problems/find-the-winner-of-the-circular-game/solutions/1152585/o-n-o-klg-n-with-table-explanation/

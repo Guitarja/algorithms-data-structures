d24cd544ccc0cdbdf7ecea24ad7d26dfba0809ae
Guitarja <124254388@qq.com>
2023-10-14 13:29:48 -0500

    Time: 197 ms (64.69%), Space: 22.9 MB (87.85%) - LeetHub

--- 1271-web-crawler/1271-web-crawler.py
+++ 1271-web-crawler/1271-web-crawler.py
@@ -25,3 +25,11 @@ class Solution:
                     visited.add(next_url)
         
         return visited
+
+# Complexity Analysis
+# Let nnn be the total number of URLs (urls.length), mmm be the number of edges in the graph, and lll be the maximum length of a URL (urls[i].length).
+# Let k be the number of traversed vertices. We add each of these vertices to the set and to the queue in up to O(l) per vertex. The total time for inserting into the set and into the queue is thus O(k⋅l)
+# The total number of elements in htmlParser.getUrls(url) over all URLs is mmm – the total number of edges in the graph. Each element can have a length of O(l)O(l)O(l). The sum of lengths of the elements of htmlParser.getUrls(url) over all URLs is O(m⋅l)O(m \cdot l)O(m⋅l).
+# The total complexity is O(k⋅l+m⋅l)O(k \cdot l + m \cdot l)O(k⋅l+m⋅l). Since k=O(m)k = O(m)k=O(m), we can simplify this expression to O(m⋅l)O(m \cdot l)O(m⋅l).
+# Time complexity: O(m⋅l)
+# Space complexity: O(n⋅l)

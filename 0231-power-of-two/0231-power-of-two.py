71940e7c14080f469077f1b53ca5a107d14ba5ab
Guitarja <124254388@qq.com>
2023-10-21 17:51:36 -0500

    Time: 48 ms (11.36%), Space: 16.2 MB (71.89%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.py
+++ 0231-power-of-two/0231-power-of-two.py
@@ -6,3 +6,8 @@ class Solution:
             n = n >> 1
         
         return ones == 1
+# idea is that a power of two in binary form has and only has one "1".
+# 1=(00000001) 
+# 2=(00000010)22 = (0000 0010)_22=(00000010) 
+# 4=(00000100)24 = (0000 0100)_24=(00000100) 
+# 8=(00001000)28 = (0000 1000)_28=(00001000)

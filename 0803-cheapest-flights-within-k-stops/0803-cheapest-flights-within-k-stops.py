5e3820f93e4343b5ae66e021485bc5a5dca31fc8
Guitarja <124254388@qq.com>
2023-10-26 11:26:26 -0500

    Time: 110 ms (61.95%), Space: 17.7 MB (50.89%) - LeetHub

---
+++ 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.py
@@ -0,0 +1,18 @@
+class Solution:
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, K: int) -> int:
+        graph = collections.defaultdict(dict)
+        for s, d, w in flights:
+            graph[s][d] = w
+        pq = [(0, src, K+1)]
+        vis = [0] * n
+        while pq:
+            w, x, k = heapq.heappop(pq)
+            if x == dst:
+                return w
+            if vis[x] >= k:
+                continue
+            vis[x] = k
+            for y, dw in graph[x].items():
+                heapq.heappush(pq, (w+dw, y, k-1))
+        return -1
+# maintain a n-size heap and time complexity is O((m + n)logn). m is number of edges and n is number of nodes. And it can be improved to O(m + nlogn) with a Fibonacci heap where a delete min costs logn but an update cost costs constant time.

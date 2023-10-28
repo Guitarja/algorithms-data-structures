64058937e21871dbcde918afa34223eb1b8ef4d9
Guitarja <124254388@qq.com>
2023-10-28 17:54:38 -0500

    Time: 632 ms (19.71%), Space: 17.5 MB (47.59%) - LeetHub

--- 1903-design-most-recently-used-queue/1903-design-most-recently-used-queue.py
+++ 1903-design-most-recently-used-queue/1903-design-most-recently-used-queue.py
@@ -1,24 +1,68 @@
-from sortedcontainers import SortedList
+class Fenwick: 
+    def __init__(self, n: int): 
+        self.nums = [0]*(n+1)
+        
+    def sum(self, k: int) -> int: 
+        ans = 0
+        while k: 
+            ans += self.nums[k]
+            k -= k & -k
+        return ans 
+    
+    def add(self, k: int, x: int) -> int: 
+        k += 1
+        while k < len(self.nums): 
+            self.nums[k] += x
+            k += k & -k 
+
 
 class MRUQueue:
 
     def __init__(self, n: int):
-        self.sl = SortedList((i-1, i) for i in range(1, n+1))
-    
+        self.size = n 
+        self.tree = Fenwick(n+2000) # buffer for 2000 calls
+        self.vals = [0]*(n+2000)
+        for i in range(n):
+            self.tree.add(i, 1)
+            self.vals[i] = i+1
+
     def fetch(self, k: int) -> int:
-	    # 1. Find the kth item
-        _, item = self.sl[k-1]
-        # 2. Find the helper element of the last item in the SortedList
-        lastItemHelper, _ = self.sl[-1]
-		# 3. Remove the kth item
-        del self.sl[k-1]
-		# 4. Place the removed item last in the SortedList
-        self.sl.add((lastItemHelper+1, item))
-        return item
+        lo, hi = 0, self.size
+        while lo < hi: 
+            mid = lo + hi >> 1 
+            if self.tree.sum(mid) < k: lo = mid + 1
+            else: hi = mid 
+        self.tree.add(lo-1, -1)
+        self.tree.add(self.size, 1)
+        self.vals[self.size] = self.vals[lo-1]
+        self.size += 1
+        return self.vals[lo-1]
 
+'''
+    Things to note:
+    1. We need a fast way to find the kth element, brute force way is O(N), so
+    it should be better than that.
+    2. Elements need to put at the back efficiently
 
-# Your MRUQueue object will be instantiated and called as such:
-# obj = MRUQueue(n)
-# param_1 = obj.fetch(k)
+    
+    SOLUTION 1: BINARY INDEXED TREE
+    
+    TC: O(log(T) * log(T))
+    SC: O(T)
 
-# O(log(n)) fetch and O(n*log(n)) constructor
+    where T = n + 2000 + 1, n = initial size of array
+
+    To find out the kth element, we can use cummulative sum of elements. In the
+    cummulative sum, each element contributes 1, i's not their value which is used
+    for the sum, rather their frequency.
+    Idea is to use binary search in the range of elements and for the nums[mid], 
+    check if it's cummulative sum till that point is k.
+
+    Once we move the element to the back, we need to update the cummulative sum as well.
+    So we can use either a segment tree or binary indexed tree for this.
+    Here in this solution a Binary Indexed tree is used. Initially we update the sum
+    for 1..n. Then for each fetch, when we move the element to back, we do 2 update calls.
+    One update(kth_idx, -1) which effectively removes contribution of kth element
+    and the other update(last_idx, 1), adds 1 to signify a new element has been added at the
+    back.
+'''
